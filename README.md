6d1c4198b2fbf60a42d915732ca25acde17d4354
Stephen Good <stephen.good@umusic.com>
2020-03-02 12:03:08 -0800

    Switched from live github http GET to cloning and building the data.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # go-legion-data
 Go module for working with the new sw-legion-data project
+
+## Requirements
+This module uses `git` and `yarn` to fetch and process the data in the `sw-legion-data` repo, please install them both.
--- data.go
+++ data.go
@@ -2,33 +2,117 @@ package legiondata
 
 import (
 	"encoding/json"
-	"fmt"
+	"gopkg.in/src-d/go-git.v4"
 	"io/ioutil"
-	"net/http"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
-const repoURL = "https://raw.githubusercontent.com/zacharyp/sw-legion-data"
+const (
+	repoURL  = "https://github.com/zacharyp/sw-legion-data"
+	repoPath = "/tmp/legion-data"
+	jsonPath = repoPath + "/lib/legion-data.json"
+)
 
 // GetData fetches the latest out/legion-data.json file, parses it and returns a Data struct
 func GetData() (Data, error) {
-	dataURL := repoURL + "/master/out/legion-data.json"
 	var data Data
-	client := http.Client{}
-	resp, err := client.Get(dataURL)
+
+	// remove cloned repo if it is already there
+	if fileExists(repoPath) {
+		err := removeDir(repoPath)
+		if err != nil {
+			log.Println("error removing repo temp directory", repoPath, err)
+			return data, err
+		}
+	}
+
+	// clone the repo
+	_, err := git.PlainClone(repoPath, false, &git.CloneOptions{
+		URL:      repoURL,
+		Progress: os.Stdout,
+	})
 	if err != nil {
 		return data, err
 	}
 
-	if resp.StatusCode != 200 {
-		return data, fmt.Errorf("non-Ok response code %s (%d)", resp.Status, resp.StatusCode)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
+	// yarn install
+	err = yarn("install", repoPath)
 	if err != nil {
 		return data, err
 	}
 
-	_ = json.Unmarshal(body, &data)
+	// yarn json
+	err = yarn("json", repoPath)
+	if err != nil {
+		return data, err
+	}
+
+	// read JSON into memory
+	bytes, err := getFileContent(jsonPath)
+	if err != nil {
+		return data, err
+	}
+	_ = json.Unmarshal(bytes, &data)
+
+	// remove cloned repo
+	err = removeDir(repoPath)
+	if err != nil {
+		// Getting this error is not a big deal.
+		// At this point we have the data, return it.
+		// But we should still log the error.
+		log.Println("error removing repo temp directory", repoPath, err)
+	}
 
 	return data, nil
+}
+
+// fileExists checks if a file exists and is not a directory before we
+// try using it to prevent further errors.
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return !os.IsNotExist(err)
+}
+
+func removeDir(dir string) error {
+	d, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+	for _, name := range names {
+		err = os.RemoveAll(filepath.Join(dir, name))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func yarn(script, path string) error {
+	cmd := exec.Command("yarn", script)
+	cmd.Dir = path
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	return err
+}
+
+func getFileContent(path string) ([]byte, error) {
+	var bytes []byte
+	file, err := os.Open(path)
+	if err != nil {
+		return bytes, err
+	}
+	defer file.Close()
+
+	bytes, _ = ioutil.ReadAll(file)
+
+	return bytes, nil
 }
